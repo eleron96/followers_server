a1ac93c6bb3b93cb75e707ccc27abbb996b1f3c8
nikogamsahurdia <eleron96@gmail.com>
2025-02-07 12:52:48 +0300

    Remove tracked files from repo

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-cd /root
-tar xzvf linkedin_project_files.tar.gz
-docker build -t linkedin_project:latest .
-docker stop linkedin_project_container || true
-docker rm linkedin_project_container || true
-docker run --name linkedin_project_container -d --restart always --shm-size=2g -p 8090:8090 --env-file .env linkedin_project:latest
